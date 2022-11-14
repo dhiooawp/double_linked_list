6b5e414b728a00f256b0140eb6daa3de72154756
DhioAWP <dhioanugerah0@gmail.com>
2022-11-14 16:55:23 +0700

    add public int and public string on class node

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -1,6 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+namespace double_linked_list
+{
+    class Node
+    {
+        /* Node class represents the node of doubly linked list.
+         * it consists of the information part and links to 
+         * its succeeding and preceeding
+         * in terms of the next and previous*/
+        public int noMhas;
+        public string name;
+    }
+}
